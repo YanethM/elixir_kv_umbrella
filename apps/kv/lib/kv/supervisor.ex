d54c13b2b122af29f5675cc702cd75c6f4fcce93
YanethM <yaneth.mejiar@autonoma.edu.co>
2022-07-20 17:19:36 -0500

    Doctest, patterns and distributed tasks

--- apps/kv/lib/kv/supervisor.ex
+++ apps/kv/lib/kv/supervisor.ex
@@ -8,35 +8,11 @@ defmodule KV.Supervisor do
   @impl true
   def init(:ok) do
     children = [
-      # {KV.Registry, name: KV.Registry},
-      # Supervisior tree
-      # {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
-
       {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
-      {KV.Registry, name: KV.Registry}
+      {KV.Registry, name: KV.Registry},
+      {Task.Supervisor, name: KV.RouterTasks}
     ]
 
-    #Supervisor.init(children, strategy: :one_for_one)
-    #el supervisor matará y reiniciará todos sus procesos secundarios cada vez que uno de ellos muera
     Supervisor.init(children, strategy: :one_for_all)
-
   end
 end
-
-# Testing commands (Not require test file)
-# Iniciar el supervisor y enumerado sus hijos. Una vez que el supervisor comenzó, también comenzó a todos sus hijos.
-# iex> KV.Registry.child_spec([])
-# iex> {:ok, sup} = KV.Supervisor.start_link([])
-# iex> Supervisor.which_children(sup)
-
-# testing a bad input on call
-# iex> [{_, registry, _, _}] = Supervisor.which_children(sup)
-# iex> GenServer.call(registry, :bad_input)
-# iex> Supervisor.which_children(sup)
-
-# iex> i(KV.Registry)
-
-# Later of adding the new children DynamicSupervisor
-# iex> {:ok, bucket} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
-# iex> KV.Bucket.put(bucket, "eggs", 3)
-# iex> KV.Bucket.get(bucket, "eggs")
--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,9 +1,14 @@
 defmodule KVServer do
   require Logger
- @doc """
-  Starts accepting connections on the given `port`.
-  """
+
   def accept(port) do
+    # The options below mean:
+    #
+    # 1. `:binary` - receives data as binaries (instead of lists)
+    # 2. `packet: :line` - receives data line by line
+    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
+    #
     {:ok, socket} = :gen_tcp.listen(port,
                       [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Accepting connections on port #{port}"
@@ -18,19 +23,40 @@ defmodule KVServer do
   end
 
   defp serve(socket) do
-    socket
-    |> read_line()
-    |> write_line(socket)
+    msg =
+      with {:ok, data} <- read_line(socket),
+           {:ok, command} <- KVServer.Command.parse(data),
+           do: KVServer.Command.run(command)
 
+    write_line(socket, msg)
     serve(socket)
   end
 
   defp read_line(socket) do
-    {:ok, data} = :gen_tcp.recv(socket, 0)
-    data
+    :gen_tcp.recv(socket, 0)
   end
 
-  defp write_line(line, socket) do
-    :gen_tcp.send(socket, line)
+  defp write_line(socket, {:ok, text}) do
+    :gen_tcp.send(socket, text)
+  end
+
+  defp write_line(socket, {:error, :not_found}) do
+    :gen_tcp.send(socket, "NOT FOUND\r\n")
+  end
+
+  defp write_line(socket, {:error, :unknown_command}) do
+    # Known error. Write to the client.
+    :gen_tcp.send(socket, "UNKNOWN COMMAND\r\n")
+  end
+
+  defp write_line(_socket, {:error, :closed}) do
+    # The connection was closed, exit politely.
+    exit(:shutdown)
+  end
+
+  defp write_line(socket, {:error, error}) do
+    # Unknown error. Write to the client and exit.
+    :gen_tcp.send(socket, "ERROR\r\n")
+    exit(error)
   end
 end
