14eb58f807e564cee0b4cc05db572c1964bbdc1e
YanethM <yaneth.mejiar@autonoma.edu.co>
2022-07-20 17:19:36 -0500

    Doctest, patterns and distributed tasks
    Update command.ex file
    
    Remove some comments
    
    Doctest, patterns and distributed tasks
    
    Staged changes

--- apps/kv/test/kv/bucket_test.exs
+++ apps/kv/test/kv/bucket_test.exs
@@ -3,18 +3,15 @@ defmodule KV.BucketTest do
 
   # se ejecuta antes de cada prueba
   setup do
-    {:ok, bucket} = KV.Bucket.start_link([])
+    bucket = start_supervised!(KV.Bucket)
     %{bucket: bucket}
   end
 
-  # %{bucket: bucket} para pasar el pid del callback a la prueb
   test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
-
-    assert KV.Bucket.delete(bucket, "milk") == 3
   end
 
   test "are temporary workers" do
